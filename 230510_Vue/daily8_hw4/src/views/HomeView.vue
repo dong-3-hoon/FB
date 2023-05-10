f3ebc2d5040698bb9c30058a7b5a3d0ee156b7b2
KSH <tpehd1009@gmail.com>
2023-05-11 00:50:16 +0900

    230510

---
+++ 230510_Vue/daily8_hw4/src/views/HomeView.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <button @click="getCatImg">Get Cat</button>
+    <br>
+    <img :src="catImgUrl" alt="">
+    <h1>CatImg</h1>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'CatView',
+  data() {
+    return {
+      catImgUrl: '',
+    };
+  },
+  methods: {
+    getCatImg(){
+      const catImageURL = `https://api.thecatapi.com/v1/images/search`
+      axios({
+        method: 'get',
+        url: catImageURL
+      })
+      .then((response)=>{
+        const catImgUrl = response.data[0].url
+        this.catImgUrl = catImgUrl
+      })
+    },
+  },
+  created(){
+    this.getCatImg()
+  },
+  watch: {
+    catImgUrl(newVal) {
+      console.log('Image URL updated:', newVal);
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
