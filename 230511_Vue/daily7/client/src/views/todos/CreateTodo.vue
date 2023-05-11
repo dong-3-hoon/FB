b31fcd4ec6ca57c8c17ca24838ba5a26623d80e3
dong-3-hoon <tpehd1009@gmail.com>
2023-05-11 14:38:30 +0900

    230511

---
+++ 230511_Vue/daily7/client/src/views/todos/CreateTodo.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <input 
+      type="text" 
+      v-model.trim="title" 
+      @keyup.enter="createTodo"
+    >
+    <button @click="createTodo">+</button>
+  </div>
+</template>
+
+<script>
+import axios from'axios'
+const API_URL = 'http://127.0.0.1:8000'
+
+export default {
+  name: 'CreateTodo',
+  data: function () {
+    return {
+      title: null,
+    }
+  },
+  methods: {
+    createTodo: function () {
+      const title = this.title
+      if(!title){
+        alert('제목을 입력하시오')
+        return
+      }
+      // 2번 문제
+      axios({
+        method: 'post',
+        url: `${API_URL}/todos/`,
+        data: {title},
+      })
+      .then(()=>{
+        this.$router.push({name: 'TodoList'})
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+    }
+  }
+}
+</script>
