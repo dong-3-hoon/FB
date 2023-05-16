17a0ff701499d256fcee739f93947aad71b36358
KSH <tpehd1009@gmail.com>
2023-05-17 00:18:38 +0900

    230517

---
+++ 230516_Vue/daily9/client/src/components/ArticleListItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h5>{{ article.id }}</h5>
+    <p>{{ article.title }}</p>
+    <p>작성자: {{article.username}}</p>
+    <hr>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ArticleListItem',
+  props: {
+    article: Object,
+  }
+}
+</script>
+
+<style>
+
+</style>
--- 230516_Vue/daily9/client/src/views/ArticlelistView.vue
+++ 230516_Vue/daily9/client/src/views/ArticlelistView.vue
@@ -1,35 +1,41 @@
 <template>
   <div>
-    <h1>게시물 목록</h1>
+    <h1>Article Page</h1>
+    <router-link :to="{ name: 'CreateView' }">[CREATE]</router-link>
+    <ArticleList />
+    <hr>
   </div>
 </template>
 
 <script>
+import ArticleList from '@/components/ArticleList.vue'
+
 export default {
-    name: 'ArticlelistView',
-    data() {
-        return{
-            articles: null,
-        }
-    },
-    methods:{
-        signup(){
-            axios({
-                method: "get",
-                url: 'http://127.0.0.1:8000/articles/',
-                data: {
-                    title: this.title,
-                    content: this.content,
-                    created_at: this.created_at,
-                },
-            })
-            .then((res)=>{
-            console.log(res.data)})
-            .catch((err)=>{
-                console.log(err)
-            })
-        }
+  name: 'ArticleView',
+  components: {
+    ArticleList,
+  },
+  computed:{
+    isLogin(){
+      return this.$store.getters.isLogin // 로그인 여부
     }
+  },
+  created() {
+    this.getArticles()
+  },
+  methods: {
+    getArticles() {
+      if(this.isLogin){
+        this.$store.dispatch('getArticles')
+      }
+      else{
+        alert('로그인이 필요한 페이지입니다.')
+        this.$router.push({name: 'LoginView'})
+      }
+      // 로그인이 되어 있으면 getArticles actio 실행
+      // 로그인 x라면 login 페이지로 이동
+    }
+  }
 }
 </script>
 
